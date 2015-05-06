26f07d6d8be812cac07f2b07c60dd054c53451f2
Oguzhan Uysal <development@oguzhanuysal.eu>
2015-05-06 07:04:41 +0200

    Updated unit test

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -1,13 +1,29 @@
 <?php
+use GuzzleHttp\Subscriber\Mock;
+use GuzzleHttp\Message\Response;
+
 use pbxg33k\VocaDB;
 
 class ClientTest extends PHPUnit_Framework_TestCase
 {
 	protected $client;
+	protected $mockServer;
 
 	protected function setUp()
 	{
 		$this->client = new VocaDB\Client();
+		$this->mockServer = new Mock();
+		$this->client->_client->getEmitter()->attach($this->mockServer);
+	}
+
+	public function testTestServer()
+	{
+		// Server::enqueue("HTTP/1.1 200 OK\r\n\Content-Length: 0r\n\r\n");
+		$this->mockServer->addResponse("HTTP/1.1 200 OK\r\n\Content-Length: 0r\n\r\n");
+		$this->assertEquals($this->client->get('/foo')->getStatusCode(), 200);
+
+		$this->mockServer->addResponse(new Response(202));
+		$this->assertEquals($this->client->get('/foo')->getStatusCode(), 202);
 	}
 
 	public function testInit()
