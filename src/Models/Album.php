fcdd352253e0c449204ca1f3aed05a419675da06
Oguzhan Uysal <development@oguzhanuysal.eu>
2015-05-06 04:12:38 +0200

    Added Album

---
+++ src/Models/Album.php
@@ -0,0 +1,161 @@
+<?php
+namespace pbxg33k\VocaDB\Models;
+
+class Album extends Base
+{
+	/**
+	 * 
+	 * @var string
+	 */
+	public $additionalNames;
+
+	/**
+	 * 
+	 * @var pbxg33k\VocaDB\Models\Collections\ArtistCollection
+	 */
+	public $artists;
+
+	/**
+	 * 
+	 * @var string
+	 */
+	public $artistString;
+
+	/**
+	 * 
+	 * @var string
+	 */
+	public $barcode;
+
+	/**
+	 * 
+	 * @var string
+	 */
+	public $catalogNumber;
+
+	/**
+	 * 
+	 * @var string
+	 */
+	public $createDate;
+
+	/**
+	 * 
+	 * @var string
+	 */
+	public $defaultName;
+
+	/**
+	 * 
+	 * @var string
+	 */
+	public $defaultNameLanguage;
+
+	/**
+	 * 
+	 * @var string
+	 */
+	public $discType;
+
+	/**
+	 * 
+	 * @var string
+	 */
+	public $id;
+
+	/**
+	 * 
+	 * @var string
+	 */
+	public $identifiers;
+
+	/**
+	 * 
+	 * @var string
+	 */
+	public $localizedName;
+
+	/**
+	 * 
+	 * @var string
+	 */
+	public $mainPicture;
+
+	/**
+	 * 
+	 * @var string
+	 */
+	public $mergedTo;
+
+	/**
+	 * 
+	 * @var string
+	 */
+	public $name;
+
+	/**
+	 * 
+	 * @var array
+	 */
+	public $names;
+
+	/**
+	 * 
+	 * @var array
+	 */
+	public $pvs;
+
+	/**
+	 * 
+	 * @var float
+	 */
+	public $ratingAverage;
+
+	/**
+	 * 
+	 * @var integer
+	 */
+	public $ratingCount;
+
+	/**
+	 * 
+	 * @var string
+	 */
+	public $releaseDate;
+
+	/**
+	 * 
+	 * @var string
+	 */
+	public $releaseEvent;
+
+	/**
+	 * 
+	 * @var string
+	 */
+	public $status;
+
+	/**
+	 * 
+	 * @var string
+	 */
+	public $tags;
+
+	/**
+	 * 
+	 * @var array
+	 */
+	public $tracks;
+
+	/**
+	 * 
+	 * @var integer
+	 */
+	public $version;
+
+	/**
+	 * 
+	 * @var string
+	 */
+	public $webLinks;
+}
