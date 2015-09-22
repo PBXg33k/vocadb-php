6c7a4eda8b9001288524128e0cada3b84509398c
Oguzhan Uysal <development@oguzhanuysal.eu>
2015-09-22 20:47:04 +0200

    Added album tests

--- tests/AlbumTest.php
+++ tests/AlbumTest.php
@@ -51,7 +51,44 @@ class AlbumTest extends PHPUnit_Framework_TestCase
 		]);
 		$response = $this->client->album->get(1);
 
-		$this->assertEquals("Tripshots feat. 初音ミク", $response->artistString);
+		// $this->assertEquals("Tripshots feat. 初音ミク", $response->artistString);
+		$this->assertInstanceOf('Pbxg33k\VocaDB\Models\Album', $response);
+
+		$this->assertEquals($response->artistString, 'Tripshots feat. 初音ミク');
+		$this->assertEquals($response->createDate, '2011-01-16T00:00:00');
+		$this->assertEquals($response->defaultName, 'Synthesis');
+		$this->assertEquals($response->defaultNameLanguage, 'English');
+		$this->assertEquals($response->discType, 'Album');
+		$this->assertEquals($response->id, 1);
+		$this->assertEquals($response->name, 'Synthesis');
+		$this->assertEquals($response->ratingAverage, 4.5);
+		$this->assertEquals($response->ratingCount, 6);
+		$this->assertEquals($response->releaseDate->day, 11);
+		$this->assertEquals($response->releaseDate->formatted, '11.6.2009');
+		$this->assertEquals($response->releaseDate->isEmpty, false);
+		$this->assertEquals($response->releaseDate->month, 6);
+		$this->assertEquals($response->releaseDate->year, 2009);
+		$this->assertEquals($response->status, 'Approved');
+		$this->assertEquals($response->version, 9);
+	}
+
+	public function testSingleCompleteAlbumObjects()
+	{
+		$this->client = new VocaDB\Client([
+			'guzzle' => [
+				'handler' => HandlerStack::create(
+					new MockHandler([
+						new Response(200, [], file_get_contents(dirname(__FILE__).'/mock/album/complete_1.json'))
+					])
+				)
+			]
+		]);
+		$response = $this->client->album->get(1);
+
+		$this->assertInstanceOf('pbxg33k\VocaDB\Models\Album', $response);
+		$this->assertInstanceOf('Pbxg33k\VocaDB\Models\Collections\ArtistCollection', $response->artists);
+		// $this->assertInstanceOf('Pbxg33k\VocaDB\Models\ReleaseEvent')
+		$this->assertInstanceOf('Pbxg33k\VocaDB\Models\Collections\TrackCollection', $response->tracks);
 	}
 	// public function testTestServer()
 	// {
