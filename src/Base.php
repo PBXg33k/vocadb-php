1568c4e33a9ba3f33b3d0363840d32c5610d174b
Oguzhan Uysal <development@oguzhanuysal.eu>
2015-09-22 23:10:18 +0200

    Splitted get() into get() and single()

--- src/Base.php
+++ src/Base.php
@@ -10,42 +10,65 @@ class Base
 		$this->client = $client;
 	}
 
-	public function get($arguments = null, $single = false)
+	public function getComplete($arguments = null)
+	{
+		return $this->get(array_merge((array)$arguments, ['fields' => implode(',', $this->fields)])));
+	}
+
+	public function get($arguments = null)
 	{
 		$full_class = explode('\\',get_called_class());
 		$class = end($full_class);
 		$model_class_name = sprintf('Pbxg33k\\VocaDB\\Models\\%s', $class);
 		$model_collection_name = sprintf('Pbxg33k\\VocaDB\\Models\\Collections\\%sCollection', $class);
         
-        $endpoint = $this->endpoint;
-        if(is_numeric($arguments)) {
-            $endpoint .= '/'.$arguments;
-            $arguments = [];
-            $single = true;
-        }
+		$response = $this->client->get($this->endpoint, $arguments);
+		switch($response->getStatusCode()) {
+			case 200:
+				// $response_obj = $response->json();
+				$response_obj = (array)json_decode($response->getBody());
+                $collection = new $model_collection_name();
+                if(count($response_obj['items'])) {
+                    foreach($response_obj['items'] as $item) {
+                        $obj = new $model_class_name();
+                        $obj->fromApi($item);
+                        $collection->add($obj);
+                    }
+                } else {
+                    throw new \Exception('No results');
+                }
+                
+				return $collection;
+				break;
+			default:
+				throw new \Exception('HTTP ERROR OCCURED');
+				break;
+		}
+	}
+
+	public function singleComplete($id, $arguments = null)
+	{
+		return $this->single($id, array_merge((array)$arguments, ['fields' => implode(',', $this->fields)]));
+	}
+
+	public function single($id, $arguments = null)
+	{
+		$full_class = explode('\\',get_called_class());
+		$class = end($full_class);
+		$model_class_name = sprintf('Pbxg33k\\VocaDB\\Models\\%s', $class);
+		$model_collection_name = sprintf('Pbxg33k\\VocaDB\\Models\\Collections\\%sCollection', $class);
+		
+		$endpoint = $this->endpoint.'/'.$id;
 
 		$response = $this->client->get($endpoint, $arguments);
 		switch($response->getStatusCode()) {
 			case 200:
 				// $response_obj = $response->json();
 				$response_obj = (array)json_decode($response->getBody());
-                if($single) {
-                    $collection = new $model_class_name();
-                    $collection->fromApi($response_obj);
-                }else{
-                    $collection = new $model_collection_name();
-                    if(count($response_obj['items'])) {
-                        foreach($response_obj['items'] as $item) {
-                            $obj = new $model_class_name();
-                            $obj->fromApi($item);
-                            $collection->add($obj);
-                        }
-                    } else {
-                        throw new \Exception('No results');
-                    }
-                }
-                
+                $collection = new $model_class_name();
+                $collection->fromApi($response_obj);
 				return $collection;
+
 				break;
 			default:
 				throw new \Exception('HTTP ERROR OCCURED');
