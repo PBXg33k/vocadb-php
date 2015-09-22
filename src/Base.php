4a1618425c8a0a70281cc3a154cd52be1b0aa283
Oguzhan Uysal <development@oguzhanuysal.eu>
2015-09-22 20:25:23 +0200

    Base class updated to work with single items

--- src/Base.php
+++ src/Base.php
@@ -10,27 +10,40 @@ class Base
 		$this->client = $client;
 	}
 
-	public function get($arguments = null)
+	public function get($arguments = null, $single = false)
 	{
 		$full_class = explode('\\',get_called_class());
 		$class = end($full_class);
 		$model_class_name = sprintf('Pbxg33k\\VocaDB\\Models\\%s', $class);
 		$model_collection_name = sprintf('Pbxg33k\\VocaDB\\Models\\Collections\\%sCollection', $class);
+        
+        $endpoint = $this->endpoint;
+        if(is_numeric($arguments)) {
+            $endpoint .= '/'.$arguments;
+            $arguments = [];
+            $single = true;
+        }
 
-		$response = $this->client->get($this->endpoint, $arguments);
+		$response = $this->client->get($endpoint, $arguments);
 		switch($response->getStatusCode()) {
 			case 200:
-				$collection = new $model_collection_name();
-				$response_obj = $response->json();
-				if(count($response_obj['items'])) {
-					foreach($response_obj['items'] as $item) {
-						$obj = new $model_class_name();
-						$obj->fromApi($item);
-						$collection->add($obj);
-					}
-				} else {
-					throw new \Exception('No results');
-				}
+				// $response_obj = $response->json();
+				$response_obj = (array)json_decode($response->getBody());
+                if($single) {
+                    $collection = new $model_class_name();
+                    $collection->fromApi($response_obj);
+                }else{
+                    $collection = new $model_collection_name();
+                    if(count($response_obj['items'])) {
+                        foreach($response_obj['items'] as $item) {
+                            $obj = new $model_class_name();
+                            $obj->fromApi($item);
+                            $collection->add($obj);
+                        }
+                    } else {
+                        throw new \Exception('No results');
+                    }
+                }
 				return $collection;
 				break;
 			default:
