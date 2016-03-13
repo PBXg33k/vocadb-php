1aca03c34585ef777aec5f073fa881b21188b128
Oguzhan Uysal <development@oguzhanuysal.eu>
2016-03-13 18:33:06 +0100

    Updated the conversion logic to a trait and applied fixes where it can now convert to classes by giving the value to the constructor

--- src/Models/Base.php
+++ src/Models/Base.php
@@ -1,9 +1,11 @@
 <?php
 namespace Pbxg33k\VocaDB\Models;
 
-class Base 
+use Pbxg33k\VocaDB\Traits\CanConvertFromStdClass;
+
+class Base
 {
-    private $nonObjectTypes = ['string', 'int', 'integer', 'bool', 'boolean', 'array', 'float'];
+    use CanConvertFromStdClass;
     /**
      * Converts an object from the api to model(s) loaded in current context
      * 
@@ -18,75 +20,7 @@ class Base
             }
         }
 
-        $this->convertFromStdClass($response);
+        $this->fromClass($response);
         return $this;
-    }
-
-    /**
-     * Converts a stdClass to models loaded in current context
-     * @param  object $class class
-     * @return object Models extending Base (and \Phalcon\Mvc\Model)
-     */
-    public function convertFromStdClass($class)
-    {
-        // Use PHP's own reflection to get the model's public properties
-        $reflection = new \ReflectionClass($this);
-        $properties = $reflection->getProperties(\ReflectionProperty::IS_PUBLIC);
-
-        // Iterate through all public properties
-        foreach ($properties as $property) {
-            $property_name = $property->name;
-            
-            if(is_object($class)) {
-            	$class_property_name = @$class->$property_name;
-            } elseif(is_array($class)) {
-            	$class_property_name = @$class[$property_name];
-            }
-
-            // Check if the current property if provided in the request payload
-            if (isset($class_property_name)) {
-                // Use the DocBlock to determine what Object we need to assign
-                if ($dummy_class = $this->getClassFromDocComment($property->getDocComment())) {
-                    // Check if it's a class or not
-                    if (in_array(strtolower(trim($dummy_class)), $this->nonObjectTypes)) {
-                        // var_dump($dummy_class, $property_name, $class, $this); die();
-                        $this->$property_name = $class_property_name;
-                    } else {
-                        // Check if the class exist
-                        if (class_exists($dummy_class)) {
-                            if (!is_null($class_property_name) && is_numeric($class_property_name)) {
-                                // Property value is an numerical value, try to use it as identifier and lookup the record in the database
-                                $this->$property_name = $dummy_class::findFirst($class_property_name);
-                            } else {
-                                $obj = new $dummy_class();
-                                $obj->convertFromStdClass($class_property_name);
-                                // We're done. Assign the result to the propery of $this
-                                $this->$property_name = $obj;
-                                unset($obj);
-                            }
-                        }
-                    }
-                } else {
-                    $this->$property_name = $class_property_name;
-                }
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * Tries to get the correct class name from the given docBlock for Reflection
-     * @param string $comment the docblock
-     * @return string|bool Classname if found match, else false
-     */
-    private function getClassFromDocComment($comment)
-    {
-        // Note: The author loves regex
-        if (preg_match('~\@var[\s]+([A-Za-z0-9\\\\]+)~', $comment, $matches)) {
-            return $matches[1];
-        }
-
-        return false;
     }
 }
---
+++ src/Traits/CanConvertFromStdClass.php
@@ -0,0 +1,248 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: PBX_g33k
+ * Date: 13-Mar-16
+ * Time: 18:17
+ */
+
+namespace Pbxg33k\VocaDB\Traits;
+
+trait CanConvertFromStdClass {
+    /**
+     * List of types which are not used as objects
+     *
+     * @var array
+     */
+    protected $nonObjectTypes = ['string', 'int', 'integer', 'bool', 'boolean', 'array', 'float'];
+
+    /**
+     * List of classes which will take string arguments in constructor
+     *
+     * @var array
+     */
+    protected $giveDataInConstructor = ['\DateTime'];
+
+    protected $objectConstructorArguments;
+
+    /**
+     * @var \ReflectionClass
+     */
+    protected $reflection;
+
+    /**
+     * @todo make this "draak van een functie" smaller by splitting up to methods en delegates.
+     *                                         ,   ,
+     *                                           $,  $,     ,
+     *                                           "ss.$ss. .s'
+     *                                   ,     .ss$$$$$$$$$$s,
+     *                                   $. s$$$$$$$$$$$$$$`$$Ss
+     *                                   "$$$$$$$$$$$$$$$$$$o$$$       ,
+     *                                  s$$$$$$$$$$$$$$$$$$$$$$$$s,  ,s
+     *                                 s$$$$$$$$$"$$$$$$""""$$$$$$"$$$$$,
+     *                                 s$$$$$$$$$$s""$$$$ssssss"$$$$$$$$"
+     *                                s$$$$$$$$$$'         `"""ss"$"$s""
+     *                                s$$$$$$$$$$,              `"""""$  .s$$s
+     *                                s$$$$$$$$$$$$s,...               `s$$'  `
+     *                            `ssss$$$$$$$$$$$$$$$$$$$$####s.     .$$"$.   , s-
+     *                              `""""$$$$$$$$$$$$$$$$$$$$#####$$$$$$"     $.$'
+     *                                    "$$$$$$$$$$$$$$$$$$$$$####s""     .$$$|
+     *                                     "$$$$$$$$$$$$$$$$$$$$$$$$##s    .$$" $
+     *                                      $$""$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"   `
+     *                                     $$"  "$"$$$$$$$$$$$$$$$$$$$$S""""'
+     *                                ,   ,"     '  $$$$$$$$$$$$$$$$####s
+     *                                $.          .s$$$$$$$$$$$$$$$$$####"
+     *                    ,           "$s.   ..ssS$$$$$$$$$$$$$$$$$$$####"
+     *                    $           .$$$S$$$$$$$$$$$$$$$$$$$$$$$$#####"
+     *                    Ss     ..sS$$$$$$$$$$$$$$$$$$$$$$$$$$$######""
+     *                     "$$sS$$$$$$$$$$$$$$$$$$$$$$$$$$$########"
+     *              ,      s$$$$$$$$$$$$$$$$$$$$$$$$#########""'
+     *              $    s$$$$$$$$$$$$$$$$$$$$$#######""'      s'         ,
+     *              $$..$$$$$$$$$$$$$$$$$$######"'       ....,$$....    ,$
+     *               "$$$$$$$$$$$$$$$######"' ,     .sS$$$$$$$$$$$$$$$$s$$
+     *                 $$$$$$$$$$$$#####"     $, .s$$$$$$$$$$$$$$$$$$$$$$$$s.
+     *      )          $$$$$$$$$$$#####'      `$$$$$$$$$###########$$$$$$$$$$$.
+     *     ((          $$$$$$$$$$$#####       $$$$$$$$###"       "####$$$$$$$$$$
+     *     ) \         $$$$$$$$$$$$####.     $$$$$$###"             "###$$$$$$$$$   s'
+     *    (   )        $$$$$$$$$$$$$####.   $$$$$###"                ####$$$$$$$$s$$'
+     *    )  ( (       $$"$$$$$$$$$$$#####.$$$$$###' -Tua Xiong     .###$$$$$$$$$$"
+     *    (  )  )   _,$"   $$$$$$$$$$$$######.$$##'                .###$$$$$$$$$$
+     *    ) (  ( \.         "$$$$$$$$$$$$$#######,,,.          ..####$$$$$$$$$$$"
+     *   (   )$ )  )        ,$$$$$$$$$$$$$$$$$$####################$$$$$$$$$$$"
+     *   (   ($$  ( \     _sS"  `"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$S$$,
+     *    )  )$$$s ) )  .      .   `$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"'  `$$
+     *     (   $$$Ss/  .$,    .$,,s$$$$$$##S$$$$$$$$$$$$$$$$$$$$$$$$S""        '
+     *       \)_$$$$$$$$$$$$$$$$$$$$$$$##"  $$        `$$.        `$$.
+     *           `"S$$$$$$$$$$$$$$$$$#"      $          `$          `$
+     *               `"""""""""""""'         '           '           '
+     *
+     *
+     *
+     * Converts a stdClass to models loaded in current context
+     *
+     * This method iterates over the passed $class
+     * For each key, it looks for a setter and type.
+     * If the value is an object, it initializes the object and assignes the initialized object.
+     *
+     * For doctrine entities, it tries to use the entity manager to retrieve entities from the database
+     *
+     * @param  object $class class
+     * @return object
+     */
+    public function fromClass($class)
+    {
+        $reflection = new \ReflectionClass($this);
+
+        // Iterate over $class for properties
+        foreach($class as $itemKey => $itemValue) {
+            // Check if key exists in $this
+
+            // Convert key to a propertyname in $this
+            $propertyName = lcfirst(str_replace(' ', '', ucwords(str_replace('_', ' ', $itemKey))));
+            $propertyName = (property_exists($this, $propertyName)) ? $propertyName :
+                (
+                property_exists($this, lcfirst($propertyName)) ? lcfirst($propertyName) :
+                    preg_replace_callback('/([A-Z])/', function($match) { return strtolower('_'.$match[1]); }, lcfirst($propertyName))
+                );
+
+            // Check if property exists and assign a ReflectionProperty class to $reflectionProperty
+            if(property_exists($this, $propertyName) && $reflectionProperty = $reflection->getProperty($propertyName)) {
+                // Get the expected property class from the property's DocBlock
+                if($propertyClassName = $this->getClassFromDocComment($reflectionProperty->getDocComment())) {
+
+                    // Set argument for constructor (if any), in case we're dealing with an object (IE: DateTime)
+                    $this->objectConstructorArguments = (in_array($propertyClassName, $this->giveDataInConstructor)) ? $itemValue : null;
+
+                    if(in_array($propertyClassName, $this->nonObjectTypes)) {
+                        $this->setPropertyValue($propertyName, $itemValue, true);
+                    } else {
+                        $object = new $propertyClassName($this->objectConstructorArguments);
+
+                        // Check if $object has valid values
+                        // IE: DateTime with a negative timestamp will cause a SQL Error
+                        $this->checkObjectForErrors($object, true);
+
+                        if($object) {
+                            // Check if object is convertable by looking for method fromClass (inherited from this trait)
+                            if(method_exists($object, 'fromClass')) {
+                                $object->fromClass($itemValue);
+                            }
+                            // We're done. Assign the result to the propery of $this
+                            $this->setPropertyValue($propertyName, $object, true);
+                        }
+                    }
+                    unset($object);
+                }
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Sets the property value for the object
+     *
+     * @param string $key
+     * @param mixed $value
+     * @param bool $isCollection Boolean to indicate Doctrine Collections
+     * @return Object
+     */
+    private function setPropertyValue($key, $value, $override = false, $isCollection = false)
+    {
+        // Replace empty strings with null
+        if($value === "")
+            $value = null;
+
+        // Convert key to method names
+        $methodName = $this->getMethodName($key);
+
+        if(!$override && method_exists($this, 'get'.$methodName)) {
+            $currentValue = $this->{'get'.$methodName}();
+            if($currentValue === $value)
+                return;
+        }
+
+        if($isCollection) {
+            if(method_exists($this, 'add'.$methodName)) {
+                return $this->{'add'.$methodName}($value);
+            } else {
+                throw new \Exception("Unable to add value, method not found: add{$methodName}. Class ".static::class);
+            }
+        } else {
+            if(method_exists($this, 'set'.$methodName)) {
+                return $this->{'set'.$methodName}($value);
+            } else {
+                throw new \Exception("Unable to set value, method not found: set{$methodName}. Class ". static::class);
+            }
+        }
+    }
+
+    private function getMethodName($propertyKey)
+    {
+        return ucfirst(preg_replace_callback('/_([a-z])/', function($match) { return strtoupper($match[1]); } , $propertyKey));
+    }
+
+    /**
+     * Tries to get the correct class name from the given docBlock for Reflection
+     *
+     * @param string $comment the docblock
+     * @return string|bool Classname if a match is found, otherwise FALSE
+     */
+    private function getClassFromDocComment($comment, $includeNamespaces = true)
+    {
+        if (preg_match('~\@var[\s]+([A-Za-z0-9\\\\]+)~', $comment, $matches)) {
+            if($includeNamespaces)
+                return $matches[1];
+            else
+                return join('', array_slice(explode('\\', $matches[1]), -1));
+        }
+
+        return false;
+    }
+
+    /**
+     * Returns the target entity from Doctrine's many to many relation
+     *
+     * @param string $comment Annotation for the property
+     * @return string|bool Classname if a match is found, otherwise FALSE
+     */
+    private function getEntityFromDoctrineManyToMany($comment)
+    {
+        if(preg_match('~@ORM\\\\ManyToMany\((:.*?)?targetEntity=\"([A-Za-z0-9\\\\]+)\"~', $comment, $matches)) {
+            return $matches[2];
+        }
+
+        return false;
+    }
+
+    private function getJoinColumnFromDoctrineAnnotation($comment)
+    {
+        if(preg_match('~@ORM\\\\JoinColumn\((:.*?)?name=\"([a-zA-Z0-9_]+)\"~', $comment, $matches)) {
+            return $matches[2];
+        }
+
+        return false;
+    }
+
+    /**
+     * Checks (and fixes) objects against known errors
+     *
+     * @param Object &$object
+     * @param bool $fix Fix errors
+     * @return Object
+     */
+    private function checkObjectForErrors(&$object, $fix = false)
+    {
+        if($object instanceof \DateTime) {
+            // The constructor (passed from the API) is NULL, indicating an empty value
+            // PHP DateTime's default value is now()
+            if($this->objectConstructorArguments == NULL) {
+                $object = NULL;
+            } else if(!$object->getTimestamp()) {
+                // DateTime has a negative or false value
+                if($fix)
+                    $object->setTimestamp(0);
+            }
+        }
+    }
+}
